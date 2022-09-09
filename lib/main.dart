c5c4b75b10460c747bcaeb9be4dbbb2dd0b928f7
Aung Ko Man <aungkoman@gmail.com>
2022-09-09 23:40:21 +0630

    user provider injected

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:myan_quiz/providers/user_provider.dart';
 import 'package:myan_quiz/view/splash_screen_page.dart';
+import 'package:provider/provider.dart';
 
 void main()
 {
+  runApp(
+      MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => UserProvider()),
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: SplashScreenPage(),
+          )
+      )
+  );
+  /*
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreenPage(),
     )
   );
+   */
 }
