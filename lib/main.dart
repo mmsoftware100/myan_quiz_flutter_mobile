da53f23bed29b29088a8826dd3d880a50e7699d0
Aung Ko Man <aungkoman@gmail.com>
2022-09-09 23:48:17 +0630

    game play provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myan_quiz/providers/game_play_provider.dart';
 import 'package:myan_quiz/providers/user_provider.dart';
 import 'package:myan_quiz/view/splash_screen_page.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +10,7 @@ void main()
       MultiProvider(
           providers: [
             ChangeNotifierProvider(create: (_) => UserProvider()),
+            ChangeNotifierProvider(create: (_) => GamePlayProvider()),
           ],
           child: MaterialApp(
             debugShowCheckedModeBanner: false,
