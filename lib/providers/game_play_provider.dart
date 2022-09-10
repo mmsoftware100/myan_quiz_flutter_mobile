a30eb572289814b50ccc3a5c8ec401c9c06a57d9
Aung Ko Man <aungkoman@gmail.com>
2022-09-10 09:17:43 +0630

    question and answer added to game play provider

--- lib/providers/game_play_provider.dart
+++ lib/providers/game_play_provider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:myan_quiz/domain/entities/answer.dart';
 import 'package:myan_quiz/domain/entities/category.dart';
+import 'package:myan_quiz/domain/entities/description.dart';
 import 'package:myan_quiz/domain/entities/user.dart';
 
 import '../domain/entities/game_type.dart';
+import '../domain/entities/question.dart';
 
 class GamePlayProvider extends ChangeNotifier{
   /* list of data */
@@ -24,8 +27,11 @@ class GamePlayProvider extends ChangeNotifier{
   // data repo
   List<GameType> gameTypes = [];
   GameType gameType = GameType.sample;
+
   List<Category> categories = [];
   Category category = Category.sample;
+
+  Question question = Question.sample;
 
 
   // methods
@@ -61,6 +67,55 @@ class GamePlayProvider extends ChangeNotifier{
   void setCategory(Category categoryUpdate){
     category = categoryUpdate;
     notifyListeners();
+  }
+
+
+  Future<bool> selectQuestionByCategoryId({required String accessToken, required int gameTypeId, required int categoryId}){
+    bool status = true;
+    return Future.delayed(Duration(seconds: 5),(){
+      question = Question(
+          id: 1,
+          name: "အိုးစားဖက်တွင် စုစုပေါင်း ______ ဉီး ပါဝင်သည်။",
+          image: "https://myanquiz.com/images/game_play_one.jpg",
+          answers: [
+            Answer(id: 1, name:  "၆ ဉီး"),
+            Answer(id: 2, name: "၅ ဉီး"),
+            Answer(id: 3, name: "၇ ဉီး"),
+            Answer(id: 4, name: "၄ ဉီး"),
+          ],
+          description: Description(
+            id: 1,
+            answerId: 4,
+            name: "အိုးစားဖက်တွင် (၄) ဉီး ပါဝင်သည်"
+          )
+      );
+      notifyListeners();
+      return status;
+    });
+  }
+  Future<User> answer({required String accessToken, required int gameTypeId, required int questionId, required int answerId}){
+    return Future.delayed(Duration(seconds: 5),(){
+      User user = User(
+          id: 1,
+          name: "name",
+          rank: "rank",
+          level: "level",
+          nextLevel: "nextLevel",
+          levelProgress: 50,
+          questionTotal: 150,
+          questionPassed: 110,
+          questionFailed: 40,
+          coinTotal: 450,
+          coinExchanged: 201,
+          coinCurrent: 125,
+          createdAt: DateTime.now(),
+          modifiedAt: DateTime.now(),
+          createdAgo: "createdAgo",
+          modifiedAgo: "modifiedAgo",
+          accessToken: accessToken
+      );
+      return user;
+    });
   }
 
 }
