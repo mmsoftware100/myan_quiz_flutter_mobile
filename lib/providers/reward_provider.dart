0a22169d09c6dfb8b34ce17f40f14771207674f9
Aung Ko Man <aungkoman@gmail.com>
2022-09-10 09:27:16 +0630

    bill exchange rate added to reward provider

--- lib/providers/reward_provider.dart
+++ lib/providers/reward_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myan_quiz/domain/entities/answer.dart';
+import 'package:myan_quiz/domain/entities/bill_exchange_rate.dart';
 import 'package:myan_quiz/domain/entities/category.dart';
 import 'package:myan_quiz/domain/entities/description.dart';
 import 'package:myan_quiz/domain/entities/telephone_operator.dart';
@@ -12,6 +13,10 @@ class RewardProvider extends ChangeNotifier{
   // data repo
   List<TelephoneOperator> telephoneOperators = [];
   TelephoneOperator telephoneOperator = TelephoneOperator.sample;
+
+
+  List<BillExchangeRate> billExchangeRates = [];
+  BillExchangeRate billExchangeRate = BillExchangeRate.sample;
 
 
 
@@ -32,6 +37,25 @@ class RewardProvider extends ChangeNotifier{
   }
   void setTelephoneOperator(TelephoneOperator telephoneOperatorUpdate){
     telephoneOperator = telephoneOperatorUpdate;
+    notifyListeners();
+  }
+
+
+  Future<bool> selectBillExchangeRates({required String accessToken}){
+    bool status = true;
+    return Future.delayed(Duration(seconds: 5),(){
+      billExchangeRates = [
+        BillExchangeRate(id: 1, name: "Bill 1000",coin: 3000, image: "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png"),
+        BillExchangeRate(id: 2, name: "Bill 3000",coin: 9000, image: "https://pub.dev/static/hash-t3bgi1vk/img/pub-dev-logo-2x.png"),
+        BillExchangeRate(id: 3, name: "Bill 5000",coin: 15000, image: "https://app-cdn.clickup.com/assets/images/brand/clickup-symbol_color.svg"),
+        BillExchangeRate(id: 4, name: "Bill 10,000",coin: 30000, image: "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png"),
+      ];
+      notifyListeners();
+      return status;
+    });
+  }
+  void setBillExchangeRate(BillExchangeRate billExchangeRateUpdate){
+    billExchangeRate = billExchangeRateUpdate;
     notifyListeners();
   }
 
