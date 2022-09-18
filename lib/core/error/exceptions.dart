327dadb333fc7e260b06033da0254986a7dafefc
Aung Aung Htet <aungaunghtet6@gmail.com>
2022-09-18 23:05:30 +0630

    18.9.2022

--- lib/core/error/exceptions.dart
+++
@@ -1,7 +0,0 @@
-class ServerException implements Exception {}
-
-class CacheException implements Exception {}
-class SerializationException implements Exception {}
-
-class UnverifiedUserException implements Exception {}
-class NeedToLoginException implements Exception {}
--- lib/domain/repositories/user_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:myan_quiz/core/error/failures.dart';
-import 'package:myan_quiz/domain/entities/user.dart';
-abstract class UserRepository{
-  Future<Either<Failure, User>> login({
-    required String accessToken,
-    required String fcmToken,
-  });
-}
