0d4b7ac65990554160a76bfe5dea8afebfeddd73
Aung Ko Man <aungkoman@gmail.com>
2022-09-27 19:47:23 +0630

    barrier token added

--- lib/data/datasources/network_interface.dart
+++ lib/data/datasources/network_interface.dart
@@ -3,10 +3,10 @@ import '../../domain/entities/question.dart';
 import '../../domain/entities/user.dart';
 
 abstract class NetworkInterface{
-  Future<Map<String,dynamic>> getRequest({required String url,required Map<String,dynamic> data, String barrierToken});
-  Future<Map<String,dynamic>> postRequest({required String url,required Map<String,dynamic> data});
-  Future<Map<String,dynamic>> putRequest({required String url,required int id, required Map<String,dynamic> data});
-  Future<Map<String,dynamic>> deleteRequest({required String url,required int id});
+  Future<Map<String,dynamic>> getRequest({required String url,required Map<String,dynamic> data, String? barrierToken});
+  Future<Map<String,dynamic>> postRequest({required String url,required Map<String,dynamic> data, String? barrierToken});
+  Future<Map<String,dynamic>> putRequest({required String url,required int id, required Map<String,dynamic> data, String? barrierToken});
+  Future<Map<String,dynamic>> deleteRequest({required String url,required int id, String? barrierToken});
 }
 /*
 class NetworkInterfaceImpl implements NetworkInterface{
