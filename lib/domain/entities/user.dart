263060c7ac7c9ee277ceea7f60098caa6afb367c
Aung Ko Man <aungkoman@gmail.com>
2023-03-18 20:26:32 +0630

    user repository added

--- lib/domain/entities/user.dart
+++ lib/domain/entities/user.dart
@@ -1,7 +1,24 @@
+import 'package:myan_quiz/domain/entities/user_statistics.dart';
+
 class User{
 
   int id;
-  String name; // display name
+  String uid;
+  String name;
+  String email;
+  String password;
+  String accessToken;
+  String phone;
+  String city;
+  String age;
+  String gender;
+  String photoUrl;
+  int roleId;
+  String createdAt;
+  String updatedAt;
+  UserStatistics userStatistics;
+
+  /*
   String rank; // rank name
   String level; // level name
   String nextLevel; // next level name
@@ -18,9 +35,25 @@ class User{
   String modifiedAgo;
   String accessToken;
 
+   */
+
   User({
     required this.id,
+    required this.uid,
     required this.name,
+    required this.email,
+    required this.password,
+    required this.accessToken,
+    required this.phone,
+    required this.city,
+    required this.age,
+    required this.gender,
+    required this.photoUrl,
+    required this.roleId,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.userStatistics,
+    /*
     required this.rank,
     required this.level,
     required this.nextLevel,
@@ -36,11 +69,30 @@ class User{
     required this.createdAgo,
     required this.modifiedAgo,
     required this.accessToken
+
+     */
   });
 
 
 
   static User get sample{
-    return User(id: 0, name: "name", rank: "rank", level: "level", nextLevel: "nextLevel", levelProgress: 0, questionTotal: 0, questionPassed: 0, questionFailed: 0, coinTotal: 0, coinExchanged: 0, coinCurrent: 0, createdAt: DateTime.now(), modifiedAt: DateTime.now(), createdAgo: "createdAgo", modifiedAgo: "modifiedAgo", accessToken: "accessToken");
+    //return User(id: 0, name: "name", rank: "rank", level: "level", nextLevel: "nextLevel", levelProgress: 0, questionTotal: 0, questionPassed: 0, questionFailed: 0, coinTotal: 0, coinExchanged: 0, coinCurrent: 0, createdAt: DateTime.now(), modifiedAt: DateTime.now(), createdAgo: "createdAgo", modifiedAgo: "modifiedAgo", accessToken: "accessToken");
+    return User(
+        id: 0,
+        uid: "uid",
+        name: "name",
+        email: "email",
+        password: "password",
+        accessToken: "accessToken",
+        phone: "phone",
+        city: "city",
+        age: "age",
+        gender: "gender",
+        photoUrl: "photoUrl",
+        roleId: 0,
+        createdAt: "createdAt",
+        updatedAt: "updatedAt",
+        userStatistics: UserStatistics.sample
+    );
   }
 }
